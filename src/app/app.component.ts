205ef9d6709d21db30ee8a30b3a388d52086a43e
Kirill Petrov <k.petrov@studytube.nl>
2023-10-24 16:34:27 +0300

    add todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-
+    <app-todos></app-todos>
     <!-- <app-child></app-child>
     <app-child2 *ngIf="myColor == 'green'"></app-child2>
     <button (click)="paintMyApp()">Change color</button> -->
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ChangeDetectionStrategy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-todos',
   template: `
     <p>
-      todos works!
+      add todo
     </p>
+    <input (keydown.enter)="addTodo($event)">
+    <!-- <button (click)=""></button> -->
+    <!-- <br>
+    <input (keyup)="onKey($event)">
+    <p>{{values}}</p> -->
+    <div *ngIf="this.todos">
+      {{ this.todos }}
+    </div>
+    <ul>
+      <li *ngFor="let todo in this.todos" [style.text-decoration]="textDecoration">
+        <button (click)="triggerTodo()">Done!/Undone</button>{{ todo }}
+      </li>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class TodosComponent {
+export class TodosComponent implements OnChanges {
+  textDecoration = ''
+  ngOnChanges() {
 
+  }
+
+  triggerTodo() {
+    this.textDecoration = "line-through"
+  }
+
+  todos = ['todo 1', 'todo 2'];
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) { }
+
+  addTodo(event: any) {
+    if (event.key === 'Enter') {
+      this.todos.push(event.target.value);
+      console.log(this.todos)
+      event.target.value = '';
+      this.changeDetectorRef.markForCheck();
+    }
+  }
+
+  values = '';
+
+  onKey(event: any) {
+    this.values += event.target.value + ' | ';
+  }
 }
